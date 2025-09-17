d4309e0544cacb13c181a7f56afcabb7f6de3621
SissiYuan369 <sissi.yuan0818@gmail.com>
2025-09-17 17:55:22 -0400

    changes

--- SissiYuan_week3/Assets/CameraTrigger.cs
+++ SissiYuan_week3/Assets/CameraTrigger.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class CameraTrigger : MonoBehaviour
 {
-    public Camera myCamera;
+    //public Camera myCamera;
+
+    public Camera currentCam;
+    public Camera previousCam;//Thisiswhere the previous camera goes to turn it off. 
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            myCamera.gameObject.SetActive(true);
-            Camera.main.gameObject.SetActive(false);
+            //myCamera.gameObject.SetActive(true);
+            //Camera.main.gameObject.SetActive(false);
+            previousCam.enabled = false;
+            currentCam.enabled = true;
+
+
         }
     }
 }
